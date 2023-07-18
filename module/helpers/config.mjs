7d6327d0427f4943775a746aa1a75ab70201dd33
stoneneedle <stoneneedle@gmail.com>
2023-07-18 04:37:35 +0000

    Initial commit.

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const AXD20 = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ AXD20.abilities = {
+  "str": "AXD20.AbilityStr",
+  "dex": "AXD20.AbilityDex",
+  "con": "AXD20.AbilityCon",
+  "int": "AXD20.AbilityInt",
+  "wis": "AXD20.AbilityWis",
+  "cha": "AXD20.AbilityCha"
+};
+
+AXD20.abilityAbbreviations = {
+  "str": "AXD20.AbilityStrAbbr",
+  "dex": "AXD20.AbilityDexAbbr",
+  "con": "AXD20.AbilityConAbbr",
+  "int": "AXD20.AbilityIntAbbr",
+  "wis": "AXD20.AbilityWisAbbr",
+  "cha": "AXD20.AbilityChaAbbr"
+};
